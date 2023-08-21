a6f5e8a8aa5b5ad7f596a3d77bfd993f18dbb40c
gift kimani <gift.kimani@students.moringaschool.com>
2023-08-21 10:17:08 +0300

    complete challenge 2

---
+++ number.py
@@ -0,0 +1,13 @@
+def positive_num(a, b, c):
+      numbers_count = 0
+      if a > 0:
+            numbers_count += 1
+      if b > 0:
+            numbers_count += 1
+      if c > 0:
+            numbers_count += 1
+            
+      return numbers_count == 2   
+      
+
+
